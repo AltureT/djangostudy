fa7c721969c3ca1daa60d02e95dd819f3fa55429
alturet <myalture@gmail.com>
2022-06-20 16:45:34 +0800

    模型与视图的测试功能实现，result视图测试待完善

--- polls/models.py
+++ polls/models.py
@@ -15,7 +15,8 @@ class Question(models.Model):
 
     def was_published_recently(self):
         # 是否一天内发布，计算方式：发布时间>=（当前时间-起源时间第一天）
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
